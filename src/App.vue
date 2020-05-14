406a8165af33e5fd0f1e82528194149333224275
TakahiroSono <taka.sono889@gmail.com>
2020-05-14 18:10:54 +0900

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <template>
   <div id="app">
-    <ui class="nav">
-      <router-link to="/hello" tag="li"><a class="nav_item">導入</a></router-link>
-      <router-link to="/selector" tag="li"><a class="nav_item">詳細度</a></router-link>
-      <router-link to="/responsive" tag="li"><a class="nav_item">レスポンシブ</a></router-link>
-      <router-link to="/question" tag="li"><a class="nav_item">問題</a></router-link>
-    </ui>
-    <hr>
   <router-view></router-view>
   </div>
 </template>
@@ -28,34 +21,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-.nav {
-  $up-lg: 901px; $dw-lg: 900px;
-  $up-sm: 601px; $dw-sm: 600px;
-
-  display: flex;
-  padding: 10px 30px;
-  margin: 20px 30px;
-  height: 50px;
-  list-style: none;
-  justify-content: space-around;
-  @media screen and (max-width: $dw-sm) {
-    margin: 20px 0 0;
-    padding: 0;
-    height: 60px;
-  }
-  li {
-    width: 20%;
-  }
-  &_item{
-    width: 100%;
-    line-height: 30px;
-    font-size: 20px;
-    @media screen and (max-width: $dw-sm) {
-      font-size: 18px;
-      height: 100%;
-      line-height: 60px;
-    }
-  }
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,17 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import HelloWorld from './page/HelloWorld.vue'
-import Selector from './page/Selector.vue'
 import Responsive from './page/Responsive.vue'
-import Question from './page/Question.vue'
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/hello', component: HelloWorld },
-  { path: '/selector', component: Selector },
-  { path: '/responsive', component: Responsive},
-  { path: '/question', component: Question }
+  { path: '/', component: Responsive}
 ];
 
 const router = new VueRouter({
